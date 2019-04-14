b12bbfbef962ad9583790c79ae8ecfccc868836c
alexAlice <alex@alice.si>
2019-04-14 06:02:05 +0200

    added loader for wallet view

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <notifications group="notifications" />
     <NavBar selected="wallet"/>
+    <div id="p3" v-if="JSON.stringify(state.history) == '{}'" class="mdl-progress mdl-js-progress mdl-progress__indeterminate"></div>
     <Wallet v-bind:currentPeriod="currentPeriod" />
     <QuestionModalDialog />
   </div>
@@ -12,9 +13,15 @@ import NavBar from '../components/NavBar.vue'
 import Wallet from '../components/Wallet.vue'
 import QuestionModalDialog from '../components/QuestionModalDialog.vue'
 import Blockchain from '../Blockchain.js'
+import State from '../State.js'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      state: State.state
+    }
+  },
   components: {
     Wallet,
     NavBar,
@@ -38,5 +45,9 @@ color: #497C99 !important;
 h1, h2, h3, h4 {
 font-weight: 500;
 color: #184E6D !important;
+}
+
+#p3 {
+  width: 100vw;
 }
 </style>
